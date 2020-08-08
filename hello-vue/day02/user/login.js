03f290a2e25b7bca69b07ecccf065854194c2ba6
rxj <1446339317@qq.com>
2020-08-08 19:48:46 +0800

    Initial commit

---
+++ hello-vue/day02/user/login.js
@@ -0,0 +1,9 @@
+const loginForm={
+    template:`
+        <div>
+            <h2>登陆页面</h2>
+            用户名：<input type="text"><br>
+            密码：<input type="password"><br>
+        </div>
+    `
+}
---
+++ hello-vue/day02/user/register.js
@@ -0,0 +1,10 @@
+const registerForm={
+    template:`
+        <div>
+            <h2>注册页面</h2>
+            用户名：<input type="text"><br>
+            密码：<input type="passowrd"><br>
+            确认密码：<input type="password">
+        </div>
+    `
+}
